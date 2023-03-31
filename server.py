6d5dbf77188ff2093267cdcaee268c428f8bedf8
Jared Campos <99213397+jared0215@users.noreply.github.com>
2023-03-31 18:01:03 -0400

    What happened

--- server.py
+++ server.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+app = Flask(__name__)
 
-@app.route('/')         
+
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+
+@app.route('/checkout', methods=['POST'])
 def checkout():
     print(request.form)
+    strawberry = int(request.form['strawberry'])
+    raspberry = int(request.form['raspberry'])
+    apple = int(request.form['apple'])
+    fruit_sum = strawberry + raspberry + apple
+    print(
+        f"Charging {request.form['first_name']} {request.form['last_name']} for {fruit_sum} fruits")
     return render_template("checkout.html")
 
-@app.route('/fruits')         
-def fruits():
-    return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+@app.route('/fruits')
+def fruits():
+    return render_template("fruits.html", )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
